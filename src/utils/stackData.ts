32a24129b3479f9a8c11047154ba872f74d6980e
Matheus Silva <matheus_lsilva@hotmail.com>
2022-11-17 15:10:19 -0300

    feat: update techs

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -4,39 +4,67 @@ import imgJS from '../public/static/img/stack/js.svg';
 import imgNode from '../public/static/img/stack/node.svg';
 import imgStyled from '../public/static/img/stack/styled.svg';
 import imgReact from '../public/static/img/stack/react.svg';
-import { SiPostgresql, SiRedux } from 'react-icons/si';
+import {
+    SiPostgresql,
+    SiRedux,
+    SiTypescript,
+    SiDocker,
+    SiJest,
+    SiNestjs,
+    SiExpress,
+} from 'react-icons/si';
 
 export const stackData = [
-  {
-    title: 'HTML5',
-    img: imgHTML
-  },
-  {
-    title: 'CSS3',
-    img: imgCSS
-  },
-  {
-    title: 'JavaScript',
-    img: imgJS
-  },
-  {
-    title: 'Styled Components',
-    img: imgStyled
-  },
-  {
-    title: 'ReactJS',
-    img: imgReact
-  },
-  {
-    title: 'Redux',
-    img: SiRedux
-  },
-  {
-    title: 'NodeJS',
-    img: imgNode
-  },
-  {
-    title: 'PostgreSQL',
-    img: SiPostgresql
-  }
+    {
+        title: 'HTML5',
+        img: imgHTML,
+    },
+    {
+        title: 'CSS3',
+        img: imgCSS,
+    },
+    {
+        title: 'JavaScript',
+        img: imgJS,
+    },
+    {
+        title: 'Styled Components',
+        img: imgStyled,
+    },
+    {
+        title: 'ReactJS',
+        img: imgReact,
+    },
+    {
+        title: 'Redux',
+        img: SiRedux,
+    },
+    {
+        title: 'NodeJS',
+        img: imgNode,
+    },
+    {
+        title: 'PostgreSQL',
+        img: SiPostgresql,
+    },
+    {
+        title: 'Typescript',
+        img: SiTypescript,
+    },
+    {
+        title: 'Docker',
+        img: SiDocker,
+    },
+    {
+        title: 'Jest',
+        img: SiJest,
+    },
+    {
+        title: 'NestJS',
+        img: SiNestjs,
+    },
+    {
+        title: 'Express',
+        img: SiExpress,
+    },
 ];
